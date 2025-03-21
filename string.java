baef81f7fd6d6cf15e0e399f2b451a301a6b34c0
Ameensha7 <alameenshaps@gmail.com>
2025-03-21 09:54:26 +0530

    Add files via upload

---
+++ string.java
@@ -0,0 +1,35 @@
+import java.util.*;
+class string
+{
+public static void main(String args[])
+{
+int n,i,j;
+String temp;
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter no. of strings");
+n=sc.nextInt();
+String[] str=new String[n];
+System.out.println("Enter the strings:");
+for(i=0;i<n;i++)
+{
+str[i]=sc.nextLine();
+}
+for(i=0;i<n;i++)
+{
+for(j=i+1;j<n;j++)
+{
+if(str[i].compareTo(str[j])>0)
+{
+temp=str[i];
+str[ji=str[j];
+str[j]=temp;
+}
+}
+}
+System.out.println("Sorted strings: ");
+for(i=0;i<n;i++)
+{
+System.out.println(str[i]);
+}
+}
+}
